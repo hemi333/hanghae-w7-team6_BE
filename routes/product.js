26dceddb419f4cd3b491c4e2d6f3d3fd2166c52c
swh_95 <tjdngur22@gmail.com>
2022-08-25 00:47:57 +0900

    ADD get 10list fuction
    ADD get 10list fuction

--- routes/product.js
+++ routes/product.js
@@ -6,11 +6,15 @@ const {
     getDetail,
     postProduct,
     deleteProduct,
+    getlimitProduct,
 } = require("../controllers/product.controller");
 
 router.route("/").post(postProduct).get(getAllProduct);
+router.route("/list").get(getlimitProduct);
 router.route("/:productId").get(getDetail).delete(deleteProduct);
 router.route("/search/:category").get(getCategory);
 
 module.exports = router;
+
+
 
